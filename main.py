68e5457fd3a2aabff9140584e714bbe77bbefd30
Lev <me@llistochek.ru>
2023-02-20 20:53:04 +0700

    Доработка регулярного выражения для плейлиста
    Ранее пользователи, у которых в никнейме есть символы '.' или '_'
    не могли скачать собственный плейлист

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ DEFAULT_LOG_LEVEL = 'INFO'
 TRACK_RE = re.compile(r'track/(\d+)$')
 ALBUM_RE = re.compile(r'album/(\d+)$')
 ARTIST_RE = re.compile(r'artist/(\d+)$')
-PLAYLIST_RE = re.compile(r'([\w\-]+)/playlists/(\d+)$')
+PLAYLIST_RE = re.compile(r'([\w\-._]+)/playlists/(\d+)$')
 
 FILENAME_CLEAR_RE = re.compile(r'[^\w\-\'() ]+')
 
