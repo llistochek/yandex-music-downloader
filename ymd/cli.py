bbbd9c0fddc1fc9a38c274b6639d93d45f9bd059
DR0P-database <dropdatabase.py@gmail.com>
2026-02-16 15:48:13 +0300

    Add tag/option/parametr to download liked tracks from "Мне нравится"

--- ymd/cli.py
+++ ymd/cli.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from typing import Optional, Union
 from urllib.parse import urlparse
 
-from yandex_music import Album, Playlist, Track
+from yandex_music import Album, Playlist, Track, TracksList
 
 from ymd import core
 
@@ -168,6 +168,7 @@ def main():
         metavar="<владелец плейлиста>/<тип плейлиста>",
     )
     id_group.add_argument("-u", "--url", help="URL исполнителя/альбома/трека/плейлиста")
+    id_group.add_argument("--liked-tracks", help="<Треки с отметкой \"Нравится\">", action="store_true")
 
     path_group = parser.add_argument_group("Указание пути")
     path_group.add_argument(
@@ -296,6 +297,22 @@ def main():
         track = client.tracks(args.track_id)
         result_tracks = track
         total_track_count = 1
+
+    elif args.liked_tracks:
+        def playlist_tracks_gen() -> Generator[Track]:
+            tracks_list: TracksList = client.users_likes_tracks()
+            nonlocal total_track_count
+            total_track_count = len(tracks_list)
+            tracks: list[Track] = tracks_list.fetch_tracks()
+            
+
+            for i in range(0, len(tracks), FETCH_PAGE_SIZE):
+                yield from client.tracks(
+                    [track.id for track in tracks[i : i + FETCH_PAGE_SIZE]]
+                )
+
+        result_tracks = playlist_tracks_gen()
+
     elif args.playlist_id is not None:
         user, kind = args.playlist_id.split("/")
         playlist = typing.cast(Playlist, client.users_playlists(kind, user))
